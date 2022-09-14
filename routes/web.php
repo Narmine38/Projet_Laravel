e802c7fa8852cea0964bb75c198b94204f0281be
narmine38 <cedric.atzori@le-campus-numerique.fr>
2022-09-14 11:01:13 +0200

    Mise en place des URL

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
 Route::get('/', function () {
-    return view('welcome');
+    return "Homepage";
 });
+
+Route::get('/product', function () {
+    return "Liste des produits";
+});
+
+Route::get('/product/{id}', function ($id) {
+    return "Fiche du produit " . $id;
+});
+
+Route::get('/cart', function () {
+    return "Panier";
+});
+
+
+
+
